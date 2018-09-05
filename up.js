526bc331d51e915dd5d6d53f2d53ec0a2ae84af9
Shota_Murano <smurano@mikilab.doshisha.ac.jp>
2018-09-05 22:30:09 +0900

    画像をPOSTする部分まで完成

--- up.js
+++ up.js
@@ -5,11 +5,11 @@ const fs       = require('fs');
 const upload   = multer({dest: 'photo'}); // プロジェクトルートの'uploads'ディレクトリにファイルを受け取るようにしている
 const hostname = '192.168.0.18';
 const port = 3000;
-const http = require('http');
-const server = http.createServer();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(express.static('views'));
 
 app.get('/', function(req, res) {
   res.render('ui.ejs', {title: 'File Uploading Test'});
@@ -28,52 +28,52 @@ app.listen(port, () =>{
 });
 
 // リクエストの処理
-function doRequest(req, res) {
-  switch(req.url) {
-  case '/':
-      fs.readFile('./views/ui.ejs', 'UTF-8',
-          function (err, data) {
-              res.writeHead(200, {'Content-Type': 'text/html'});
-              res.write(data);
-              res.end();
-          }
-      );
-      break;
-  case '/style.css':
-      fs.readFile('./views/style.css', 'UTF-8',
-          function (err, data) {
-              res.writeHead(200, {'Content-Type': 'text/css'});
-              res.write(data);
-              res.end();
-          }
-      );
-      break;
-  case '/material/doshisha.png':
-      fs.readFile('./views/material/doshisha.png', 'binary',
-          function (err, data) {
-              res.writeHead(200, {'Content-Type': 'image/png'});
-              res.write(data, 'binary');
-              res.end();
-          }
-      );
-      break;
-  case '/material/album.png':
-      fs.readFile('./views/material/album.png', 'binary',
-          function (err, data) {
-              res.writeHead(200, {'Content-Type': 'image/png'});
-              res.write(data, 'binary');
-              res.end();
-          }
-      );
-      break;
-  case '/material/takephoto.jpg':
-      fs.readFile('./views/material/takephoto.jpg', 'binary',
-          function (err, data) {
-              res.writeHead(200, {'Content-Type': 'image/jpg'});
-              res.write(data, 'binary');
-              res.end();
-          }
-      );
-      break;
-    }
-}
+// function doRequest(req, res) {
+//   switch(req.url) {
+//   case '/':
+//       fs.readFile('./views/ui.ejs', 'UTF-8',
+//           function (err, data) {
+//               res.writeHead(200, {'Content-Type': 'text/html'});
+//               res.write(data);
+//               res.end();
+//           }
+//       );
+//       break;
+//   case '/style.css':
+//       fs.readFile('./views/style.css', 'UTF-8',
+//           function (err, data) {
+//               res.writeHead(200, {'Content-Type': 'text/css'});
+//               res.write(data);
+//               res.end();
+//           }
+//       );
+//       break;
+//   case '/material/doshisha.png':
+//       fs.readFile('./views/material/doshisha.png', 'binary',
+//           function (err, data) {
+//               res.writeHead(200, {'Content-Type': 'image/png'});
+//               res.write(data, 'binary');
+//               res.end();
+//           }
+//       );
+//       break;
+//   case '/material/album.png':
+//       fs.readFile('./views/material/album.png', 'binary',
+//           function (err, data) {
+//               res.writeHead(200, {'Content-Type': 'image/png'});
+//               res.write(data, 'binary');
+//               res.end();
+//           }
+//       );
+//       break;
+//   case '/material/takephoto.jpg':
+//       fs.readFile('./views/material/takephoto.jpg', 'binary',
+//           function (err, data) {
+//               res.writeHead(200, {'Content-Type': 'image/jpg'});
+//               res.write(data, 'binary');
+//               res.end();
+//           }
+//       );
+//       break;
+//     }
+// }
